a6ed5c221e12078e264cb8f1f075e540f79dd3ae
MacBook <macbook@M1-Waantech.local>
2024-12-13 14:43:43 +0500

    changed : port

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.use(express.static("public"));
 
 export const prisma = new PrismaClient();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
